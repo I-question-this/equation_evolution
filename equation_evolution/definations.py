8fe749e06cc8f7c730919ca3f2b4e2f9194b81a7
Tyler Westland <tylerofthewest@gmail.com>
2019-06-03 16:22:13 -0400

    Adds pow and better evaluation.
    Adds a protected pow that so as to
    allow polynomials.
    
    Adds seperate evaluation for the benign
    and the malware portions.

--- equation_evolution/definations.py
+++ equation_evolution/definations.py
@@ -28,9 +28,15 @@ def createEnvironment(benign_equation, malware_equation, malware_xMin, malware_x
 	# Primatives
 	def protectedDiv(left, right):
 		try:
-			return left /right
+			return left / right
 		except ZeroDivisionError:
 			return 1
+
+	def protectedPow(base, exponent):
+		if base == 0:
+			return 0
+		result = base**exponent
+		return result.real if type(result) == complex else result
 
 	pset = gp.PrimitiveSet("MAIN", arity=1)
 	pset.renameArguments(ARG0='x')
@@ -38,14 +44,15 @@ def createEnvironment(benign_equation, malware_equation, malware_xMin, malware_x
 	pset.addPrimitive(operator.add, 2)
 	pset.addPrimitive(operator.sub, 2)
 	pset.addPrimitive(operator.mul, 2)
-	pset.addPrimitive(protectedDiv, 2)
+	pset.addPrimitive(protectedDiv, 2, name="div")
+	pset.addPrimitive(protectedPow, 2, name="pow")
 	pset.addPrimitive(operator.neg, 1)
 	pset.addPrimitive(math.cos, 1)
 	pset.addPrimitive(math.sin, 1)
-	pset.addEphemeralConstant("rand101", lambda: random.randint(-1,1))
+	pset.addEphemeralConstant("rand", lambda: random.randint(-1,1))
 
 	# Creator
-	creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+	creator.create("FitnessMin", base.Fitness, weights=(-1.0, -100.0))
 	creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin, pset=pset)
 
 	# Tool registration
@@ -55,23 +62,27 @@ def createEnvironment(benign_equation, malware_equation, malware_xMin, malware_x
 	toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 	toolbox.register("compile", gp.compile, pset=pset)
 	toolbox.register("manualExpr", gp.PrimitiveTree.from_string, pset=pset)
-
-	def pieceWiseFunction(x, benign_equation, malware_equation, malware_xMin, malware_xMax):
-		if malware_xMin <= x <= malware_xMax:
-			return malware_equation(x)
-		else:
-			return benign_equation(x)
-	toolbox.register("pieceWiseFunction", pieceWiseFunction, benign_equation=toolbox.compile(toolbox.manualExpr(benign_equation)), malware_equation=toolbox.compile(toolbox.manualExpr(malware_equation)), malware_xMin=malware_xMin, malware_xMax=malware_xMax)
+	
+	toolbox.register("benignEquation", lambda x, eqn: eqn(x), eqn=toolbox.compile(toolbox.manualExpr(benign_equation)))
+	toolbox.register("malwareEquation", lambda x, eqn: eqn(x), eqn=toolbox.compile(toolbox.manualExpr(malware_equation)))
+	toolbox.register("inMalwareRange", lambda x, rangeMin, rangeMax: rangeMin <= x <= rangeMax, rangeMin=malware_xMin, rangeMax=malware_xMax)
+	toolbox.register("pieceWiseFunction", lambda x: toolbox.malwareEquation(x) if toolbox.inMalwareRange(x) else toolbox.benignEquation(x))
 
 	def evalSymbReg(individual, points):
 		# Transform the tree expression in a collable function
 		func = toolbox.compile(expr=individual)
-		# Evaluate the mean squared error between the expression
-		# and the real function: x**4 - x**3 + x**2 + x
-		sqerrors = ((func(x) - toolbox.pieceWiseFunction(x))**2 for x in points)
-		return math.fsum(sqerrors) / len(points),
+		# Evaluate the mean squared errors
+		benignErrors = []
+		malwareErrors = []
+		for x in points:
+			if toolbox.inMalwareRange(x):
+				malwareErrors.append((func(x) - toolbox.malwareEquation(x))**2)
+			else:
+				benignErrors.append((func(x) - toolbox.benignEquation(x))**2)
 
-	toolbox.register("evaluate", evalSymbReg, points=[x/10. for x in range(-100,100)])
+		return math.fsum(benignErrors) / len(benignErrors), math.fsum(malwareErrors) / len(malwareErrors)
+
+	toolbox.register("evaluate", evalSymbReg, points=[x/10. for x in range(-20,20)])
 	toolbox.register("select", tools.selTournament, tournsize=3)
 	toolbox.register("mate", gp.cxOnePoint)
 	toolbox.register("expr_mut", gp.genFull, min_=0, max_=2)
@@ -96,11 +107,11 @@ def createEnvironment(benign_equation, malware_equation, malware_xMin, malware_x
 	toolbox.register("plotEquationStructure", plotEquationStructure)
 
 	def plotEquationResults(individual, output_name):
-		Xs = [x/10. for x in range(-100,100)] # -10->10 with .1 steps
+		Xs = [x/10. for x in range(-20,20)]
 		func = toolbox.compile(expr=individual)
 
 		plt.plot(Xs, [func(x) for x in Xs], 'r', label="Evolved Trojan")
-		plt.plot(Xs, [toolbox.pieceWiseFunction(x) for x in Xs], 'b', label="Piecewise Target")
+		plt.plot(Xs, [toolbox.pieceWiseFunction(x) for x in Xs], 'b--', label="Piecewise Target")
 		plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
 		plt.savefig(output_name)
 	
@@ -108,9 +119,10 @@ def createEnvironment(benign_equation, malware_equation, malware_xMin, malware_x
 
 
 	# Statistics
-	stats_fit = tools.Statistics(lambda ind: ind.fitness.values)
+	stats_benign = tools.Statistics(lambda ind: ind.fitness.values[0])
+	stats_malware = tools.Statistics(lambda ind: ind.fitness.values[1])
 	stats_size = tools.Statistics(len)
-	mstats = tools.MultiStatistics(fitness=stats_fit, size=stats_size)
+	mstats = tools.MultiStatistics(benign=stats_benign, malware=stats_malware, size=stats_size)
 	mstats.register("avg", numpy.mean)
 	mstats.register("std", numpy.std)
 	mstats.register("min", numpy.min)
